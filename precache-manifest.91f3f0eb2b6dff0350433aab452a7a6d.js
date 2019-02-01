9d2f3e32816095a534522f004e38fbcf0878062c
bharath_bheemireddy <bharath.bheemireddy@colosseumgroup.com>
2019-02-01 17:54:32 +0530

    Updates

---
+++ precache-manifest.91f3f0eb2b6dff0350433aab452a7a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "455bb57d6ec0ff8acc00",
+    "url": "/sample/static/js/runtime~main.455bb57d.js"
+  },
+  {
+    "revision": "50540634e4b547ec93da",
+    "url": "/sample/static/js/main.50540634.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/sample/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "50540634e4b547ec93da",
+    "url": "/sample/static/css/main.f2c97be4.chunk.css"
+  },
+  {
+    "revision": "d6bfe9430adea78f3951cfaea3a53742",
+    "url": "/sample/index.html"
+  }
+];
